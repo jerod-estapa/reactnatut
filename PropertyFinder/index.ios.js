8e9a23de262188212b13f361ed84b7020fa0e9de
jerodestapa <jerodestapa@gmail.com>
2015-09-10 23:40:59 -0500

    Added first object

---
+++ PropertyFinder/index.ios.js
@@ -0,0 +1,16 @@
+// React Native Tutorial
+
+'use strict';
+
+var React = require('react-native');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontsize: 30,
+    margin: 80,
+  }
+});
+
+class PropertyFinderApp extends 
